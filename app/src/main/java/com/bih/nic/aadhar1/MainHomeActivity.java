f6e33043e13101a88770a55feabba5835cd22273
patnaapp <patnaapp@gmail.com>
2020-05-19 16:16:37 -0700

    add home page

---
+++ app/src/main/java/com/bih/nic/aadhar1/MainHomeActivity.java
@@ -0,0 +1,13 @@
+package com.bih.nic.aadhar1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_home);
+    }
+}
