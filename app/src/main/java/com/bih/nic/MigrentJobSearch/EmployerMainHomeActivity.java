a353c3c3cdfa9c070cb7a7d748bb9b67014a7129
patnaapp <patnaapp@gmail.com>
2020-06-03 10:35:16 +0530

    changes

--- app/src/main/java/com/bih/nic/MigrentJobSearch/EmployerMainHomeActivity.java
+++ app/src/main/java/com/bih/nic/MigrentJobSearch/EmployerMainHomeActivity.java
@@ -21,28 +21,25 @@ import android.widget.Toast;
 import com.bih.nic.MigrentJobSearch.DataBaseHelper.DataBaseHelper;
 import com.bih.nic.MigrentJobSearch.Model.BenDetails;
 import com.bih.nic.MigrentJobSearch.Model.PaymentStatusEntity;
-import com.bih.nic.MigrentJobSearch.ui.labour.JobSearchActivity;
-import com.bih.nic.MigrentJobSearch.ui.labour.ModifyDocumentActivity;
-import com.bih.nic.MigrentJobSearch.ui.labour.PaymentStatusActivity;
-import com.bih.nic.MigrentJobSearch.ui.labour.ProfileActivity;
 import com.squareup.picasso.Picasso;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class EmployerMainHomeActivity extends Activity {
     LinearLayout ll_profile,ll_new_labours;
-    String Reg_No="",user_name="", mobile="", address="", DistName="", ProfileImg="";
+    String Reg_No="",user_name="", mobile="", address="", DistName="", ProfileImg="",workplace="";
     TextView tv_benname,urole,tv_mobile,tv_address,tv_version;
     CircleImageView profile_image;
+    TextView tv_workplace,tv_phone,tv_work_address;
     //ImageView profile_image;
 
-   BenDetails BenDetails;
+    BenDetails BenDetails;
     DataBaseHelper dataBaseHelper;
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_employer_home_new);
+        setContentView(R.layout.activity_employer_home_new);
         getActionBar().hide();
 
         BenDetails=new BenDetails();
@@ -57,6 +54,11 @@ public class EmployerMainHomeActivity extends Activity {
         tv_address=(TextView) findViewById(R.id.tv_address);
         tv_version=(TextView) findViewById(R.id.tv_version);
 
+
+        tv_workplace=(TextView) findViewById(R.id.tv_workplace);
+        tv_phone=(TextView) findViewById(R.id.tv_phone);
+        tv_work_address=(TextView) findViewById(R.id.tv_work_address);
+
         profile_image=(CircleImageView) findViewById(R.id.profile_image);
 
         String version = Utiilties.getAppVersion(this);
@@ -69,54 +71,14 @@ public class EmployerMainHomeActivity extends Activity {
         ll_profile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (!GlobalVariables.isOffline && !Utiilties.isOnline(EmployerMainHomeActivity.this)) {
 
-                    AlertDialog.Builder ab = new AlertDialog.Builder(EmployerMainHomeActivity.this);
-                    ab.setMessage(Html.fromHtml(
-                            "<font color=#000000>Internet Connection is not avaliable..Please Turn ON Network Connection </font>"));
-                    ab.setPositiveButton("Turn On Network Connection", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int whichButton) {
-                            Intent I = new Intent(android.provider.Settings.ACTION_WIRELESS_SETTINGS);
-                            startActivity(I);
-                        }
-                    });
-
-                    ab.create().getWindow().getAttributes().windowAnimations = R.style.alert_animation;
-                    ab.show();
-
-                }else{
-
-                    new FetchBenData1().execute();
-
-                }
 
             }
         });
         ll_new_labours.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (!GlobalVariables.isOffline && !Utiilties.isOnline(EmployerMainHomeActivity.this)) {
 
-                    AlertDialog.Builder ab = new AlertDialog.Builder(EmployerMainHomeActivity.this);
-                    ab.setMessage(Html.fromHtml(
-                            "<font color=#000000>Internet Connection is not avaliable..Please Turn ON Network Connection </font>"));
-                    ab.setPositiveButton("Turn On Network Connection", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(DialogInterface dialog, int whichButton) {
-                            new FetchBenData().execute();
-                            Intent I = new Intent(android.provider.Settings.ACTION_WIRELESS_SETTINGS);
-                            startActivity(I);
-                        }
-                    });
-
-                    ab.create().getWindow().getAttributes().windowAnimations = R.style.alert_animation;
-                    ab.show();
-
-                }else{
-                    new FetchBenData().execute();
-
-                }
 
             }
         });
@@ -128,52 +90,22 @@ public class EmployerMainHomeActivity extends Activity {
 
         Reg_No=PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).getString("UserId", "");
         user_name=PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).getString("UserName", "");
-        mobile=PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).getString("Mobile", "");
-        address=PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).getString("Address", "");
+
+
         DistName=PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).getString("DistName", "");
-        ProfileImg=PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).getString("Photo", "");
+        workplace=PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).getString("Photo", "");
+        mobile=PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).getString("Mobile", "");
+        address=PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).getString("Photo", "");
 
 
-        tv_benname.setText(user_name);
-        urole.setText("पंजीकरण संख्या: \n"+Reg_No);
-        tv_mobile.setText("मोबाइल नंबर: \n"+mobile);
-        tv_address.setText("  पता: "+address);
+//        tv_benname.setText(user_name);
+        tv_workplace.setText(workplace);
+        tv_phone.setText(mobile);
+        tv_work_address.setText(address);
 
-        String imagesr=dataBaseHelper.getBenImg(Reg_No);
-
-        if (imagesr!=null) {
-
-            byte[] imgData = Base64.decode(imagesr, Base64.DEFAULT);
-            Bitmap bmp = BitmapFactory.decodeByteArray(imgData, 0, imgData.length);
-            profile_image.setImageBitmap(bmp);
-
-
-            //}
-        }
-        else {
-            if(!ProfileImg.equalsIgnoreCase("NA")) {
-                String url = "http://shramsadhan.bih.nic.in" + ProfileImg.replace("~", "");
-                Log.e("imgUrl", url);
-                Picasso.with(this).load(url).into(profile_image);
-            }else {
-               // profile_image.setBackgroundResource(R.drawable.profile);
-
-            }
-        }
     }
 
-    public void onSearchJob(View view){
-        Intent i =new Intent(EmployerMainHomeActivity.this, JobSearchActivity.class);
-        i.putExtra("data",Reg_No);
-        i.putExtra("DistName",DistName);
-        startActivity(i);
-    }
-
-    public void onCheckPaymentStatus(View view){
-        new SyncPaymentStatus().execute();
-    }
-
-    public void onLogout(View view){
+    public void OnClick_goToLoginScreen(View view){
         new AlertDialog.Builder(this)
                 .setTitle("लॉग आउट ?")
                 .setMessage("क्या आप वाकई एप्लिकेशन से लॉगआउट करना चाहते हैं \n ")
@@ -216,154 +148,6 @@ public class EmployerMainHomeActivity extends Activity {
         onExit();
     }
 
-    private class FetchBenData extends AsyncTask<String, Void, BenDetails> {
-        BenDetails data;
-        String _uid;
-        private final ProgressDialog dialog = new ProgressDialog(EmployerMainHomeActivity.this);
-        private final AlertDialog alertDialog = new AlertDialog.Builder(EmployerMainHomeActivity.this).create();
-
-        @Override
-        protected void onPreExecute() {
-
-            this.dialog.setCanceledOnTouchOutside(false);
-            this.dialog.setMessage("लोड हो रहा है...");
-            if (!EmployerMainHomeActivity.this.isFinishing()) {
-                this.dialog.show();
-            }
-        }
-
-        @Override
-        protected BenDetails doInBackground(String... param) {
-
-//
-//            String res = WebServiceHelper.UploadFinalData(data, PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).getString("USERID", ""));
-//            return res;
-
-            return WebserviceHelper.getBen_Details(Reg_No);
-
-        }
-
-        @Override
-        protected void onPostExecute(BenDetails result) {
-            if (this.dialog.isShowing()) {
-                this.dialog.dismiss();
-            }
-            Log.d("Responsevalue", "" + result);
-            if (result != null) {
-                BenDetails=result;
-                Intent i =new Intent(EmployerMainHomeActivity.this, ModifyDocumentActivity.class);
-                i.putExtra("data",BenDetails);
-                startActivity(i);
 
 
-            } else {
-
-                Toast.makeText(getApplicationContext(), "Result Null..Please Try Later", Toast.LENGTH_SHORT).show();
-            }
-
-        }
-    }
-    private class FetchBenData1 extends AsyncTask<String, Void, BenDetails> {
-        BenDetails data;
-        String _uid;
-        private final ProgressDialog dialog = new ProgressDialog(EmployerMainHomeActivity.this);
-        private final AlertDialog alertDialog = new AlertDialog.Builder(EmployerMainHomeActivity.this).create();
-
-
-
-        @Override
-        protected void onPreExecute() {
-
-            this.dialog.setCanceledOnTouchOutside(false);
-            this.dialog.setMessage("लोड हो रहा है...");
-            if (!EmployerMainHomeActivity.this.isFinishing()) {
-                this.dialog.show();
-            }
-        }
-
-        @Override
-        protected BenDetails doInBackground(String... param) {
-
-            return WebserviceHelper.getBen_Details(Reg_No);
-
-        }
-
-        @Override
-        protected void onPostExecute(BenDetails result) {
-            if (this.dialog.isShowing()) {
-                this.dialog.dismiss();
-            }
-            Log.d("Responsevalue", "" + result);
-            if (result != null) {
-                BenDetails=result;
-                Intent i =new Intent(EmployerMainHomeActivity.this, ProfileActivity.class);
-                i.putExtra("data",BenDetails);
-                startActivity(i);
-
-
-            } else {
-
-                Toast.makeText(getApplicationContext(), "Result Null..Please Try Later", Toast.LENGTH_SHORT).show();
-            }
-
-        }
-    }
-
-    private class SyncPaymentStatus extends AsyncTask<String, Void, PaymentStatusEntity> {
-        BenDetails data;
-        String _uid;
-        private final ProgressDialog dialog = new ProgressDialog(EmployerMainHomeActivity.this);
-        private final AlertDialog alertDialog = new AlertDialog.Builder(EmployerMainHomeActivity.this).create();
-
-
-
-        @Override
-        protected void onPreExecute() {
-
-            this.dialog.setCanceledOnTouchOutside(false);
-            this.dialog.setMessage("लोड हो रहा है...");
-            if (!EmployerMainHomeActivity.this.isFinishing()) {
-                this.dialog.show();
-            }
-        }
-
-        @Override
-        protected PaymentStatusEntity doInBackground(String... param) {
-
-            return WebserviceHelper.getPaymentDetail(Reg_No);
-
-        }
-
-        @Override
-        protected void onPostExecute(PaymentStatusEntity result) {
-            if (this.dialog.isShowing()) {
-                this.dialog.dismiss();
-            }
-            //Log.d("Responsevalue", "" + result);
-            if (result != null) {
-                if(result.getStatus()){
-                    Intent i =new Intent(EmployerMainHomeActivity.this, PaymentStatusActivity.class);
-                    i.putExtra("data",result);
-                    startActivity(i);
-                }else{
-                    new AlertDialog.Builder(EmployerMainHomeActivity.this)
-                            .setTitle("Failed")
-                            .setMessage(result.getMsg())
-                            .setCancelable(false)
-                            .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-                                public void onClick(DialogInterface dialog, int id) {
-                                    dialog.dismiss();
-                                }
-                            })
-                            .show();
-                }
-
-
-            } else {
-
-                Toast.makeText(getApplicationContext(), "Result Null!!, OR Payment Status not found!!", Toast.LENGTH_SHORT).show();
-            }
-
-        }
-    }
 }
