85de5d5aeb28ecb719478c278c28275df5f61683
patnaapp <patnaapp@gmail.com>
2020-06-04 10:39:03 +0530

    changes

--- app/src/main/java/com/bih/nic/MigrentJobSearch/adapter/AccptedRctdJobAdapter.java
+++ app/src/main/java/com/bih/nic/MigrentJobSearch/adapter/AccptedRctdJobAdapter.java
@@ -2,6 +2,7 @@ package com.bih.nic.MigrentJobSearch.adapter;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -52,6 +53,23 @@ public class AccptedRctdJobAdapter extends RecyclerView.Adapter<AccptedRctdJobAd
         holder.tv_gender.setText(info.getTtlRegA());
         holder.tv_labour_district.setText(info.getTtlRegR());
 
+        holder.iv_call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", info.get(), null));
+//                activity.startActivity(intent);
+            }
+        });
+
+        holder.iv_call_candidate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", info.get(), null));
+//                activity.startActivity(intent);
+            }
+        });
+
+
 //        holder.tv_accpted.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View v) {
@@ -87,7 +105,7 @@ public class AccptedRctdJobAdapter extends RecyclerView.Adapter<AccptedRctdJobAd
 
     public class ViewHolder  extends RecyclerView.ViewHolder implements View.OnClickListener {
         TextView tv_regno,tv_labour_name,tv_gender,tv_labour_district,tv_labor_block,tv_person_addres,tv_age,tv_candidate_mob,tv_gauradian_name,tv_gaurdian_no,tv_skill,tv_exp_max,tv_other,tv_candid_skill;
-        ImageView iv_call;
+        ImageView iv_call_candidate,iv_call;
 
 
         Button btn_accpt,btn_rjct;
@@ -110,6 +128,8 @@ public class AccptedRctdJobAdapter extends RecyclerView.Adapter<AccptedRctdJobAd
             tv_exp_max=itemView.findViewById(R.id.tv_exp_max);
             tv_other=itemView.findViewById(R.id.tv_other);
             tv_candid_skill=itemView.findViewById(R.id.tv_candid_skill);
+            iv_call=itemView.findViewById(R.id.iv_call);
+            iv_call_candidate=itemView.findViewById(R.id.iv_call_candidate);
 
 
 
