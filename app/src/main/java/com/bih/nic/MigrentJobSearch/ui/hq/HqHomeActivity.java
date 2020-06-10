29407029f132034396d462133d83a254497f3c6b
dgrc <patnaapp@gmail.com>
2020-06-10 11:31:16 +0530

    Changes in request otp

---
+++ app/src/main/java/com/bih/nic/MigrentJobSearch/ui/hq/HqHomeActivity.java
@@ -0,0 +1,15 @@
+package com.bih.nic.MigrentJobSearch.ui.hq;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.bih.nic.MigrentJobSearch.R;
+
+public class HqHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hq_home);
+    }
+}
